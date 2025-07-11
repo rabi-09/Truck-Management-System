3197ccd38d4e7c06724ee199bd2a67593e3d06ce
rabi.09 <dasrabi2004@gmail.com>
2025-07-11 23:15:03 +0530

    MySQL Driver Connection updated

--- src/main/java/org/example/DriverConnection.java
+++ src/main/java/org/example/DriverConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverConnection {
-	private static final String USERNAME = "root", PASSWORD = "Rabi@7848", URL = "jdbc:mysql://localhost:3307/truck_management";
+	private static final String USERNAME = "your_username", PASSWORD = "your_password", URL = "jdbc:mysql://localhost:your_port_id/database_name";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
